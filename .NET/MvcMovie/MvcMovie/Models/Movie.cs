c21f72844e4ce50ab17ac36626ef83f9fc6d7c29
GSiekierski <grzegorzsiekierski7@gmail.com>
2025-04-03 22:43:22 +0200

    lab6 - Część 10. Badanie metod Szczegółów - skończone

--- .NET/MvcMovie/MvcMovie/Models/Movie.cs
+++ .NET/MvcMovie/MvcMovie/Models/Movie.cs
@@ -7,6 +7,8 @@ namespace MvcMovie.Models;
 public class Movie
 {
     public int Id { get; set; }
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
     public string? Title { get; set; }
     [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
@@ -14,4 +16,5 @@ public class Movie
     public string? Genre { get; set; }
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
+    public string? Rating { get; set; }
 }
